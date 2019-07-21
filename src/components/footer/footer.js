edd632a766879c4ffbd8fe0f87fcd99b7959bce1
salviolorenzo <salviolorenzo@gmail.com>
2019-07-21 13:42:49 -0400

    adding footer links

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,5 +3,32 @@ import { Route, Link } from 'react-router-dom';
 import './footer.scss';
 
 export default () => {
-  return <footer className="mainFooter">This will be the Footer</footer>;
+  return (
+    <footer className="mainFooter">
+      <ul>
+        <li>
+          <a href="https://www.facebook.com/uburdub">
+            <i class="fab fa-facebook-square" />
+            Facebook
+          </a>
+        </li>
+        <li>
+          <a href="https://www.facebook.com/groups/297425144206547/">
+            <i class="fab fa-facebook-square" />
+            Ubur's Long Bois
+          </a>
+        </li>
+        <li>
+          <a href="https://www.instagram.com/uburdub/">
+            <i class="fab fa-instagram" />Instagram
+          </a>
+        </li>
+        <li>
+          <a href="https://twitter.com/uburdub">
+            <i class="fab fa-twitter" />Twitter
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
 };
