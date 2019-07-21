ed702773c6ec5d1092fc414fb1ac3c31b7889d87
salviolorenzo <salviolorenzo@gmail.com>
2019-07-21 17:07:10 -0400

    adding twitterEmbed

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -4,6 +4,18 @@ import './home.scss';
 import Clip2 from './images/VJ_Clip_2_V2.mov';
 import ReactPlayer from 'react-player';
 import Products from '../products/Products';
+import {
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+  TwitterFollowButton,
+  TwitterHashtagButton,
+  TwitterMentionButton,
+  TwitterTweetEmbed,
+  TwitterMomentShare,
+  TwitterDMButton,
+  TwitterVideoEmbed,
+  TwitterOnAirButton
+} from 'react-twitter-embed';
 
 export default () => {
   return (
@@ -12,6 +24,17 @@ export default () => {
         <div className="videoPlaceHolder" />
       </div>
       <Products />
+      <div className="twitterFeed">
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="uburdub"
+          options={{ height: 500 }}
+        />
+        <TwitterFollowButton
+          screenName={'uburdub'}
+          style={{ display: 'flex', 'justify-content': 'flex-end' }}
+        />
+      </div>
     </>
   );
 };
