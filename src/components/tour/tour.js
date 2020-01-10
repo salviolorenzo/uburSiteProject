06320575407665ab75a4a9110f935c7d367ab8d9
salviolorenzo <salviolorenzo@gmail.com>
2020-01-09 19:42:34 -0500

    fixing tour component bug

--- src/components/tour/tour.js
+++ src/components/tour/tour.js
@@ -6,66 +6,87 @@ export default class Tour extends Component {
     super(props);
     this.state = {
       apiKey: 'LMt7ZxhYTWBMi7cY',
-      events: ''
+      events: []
     };
   }
 
   componentDidMount() {
-    fetch(`https://api.songkick.com/api/3.0/artists/9542144/calendar.json?apikey=${
-      this.state.apiKey
-    }
-    `)
-      .then(r => r.json())
-      .then(res => {
-        let shows = res.resultsPage.results.event;
-        this.setState({
-          events: shows
-        });
+    this.setState({
+      events: []
+    });
+    console.log(this.state);
+    // fetch(`https://api.songkick.com/api/3.0/artists/9542144/calendar.json?apikey=${this.state.apiKey}
+    // `)
+    //   .then(r => r.json())
+    //   .then(res => {
+    //     this.setState({
+    //       events: res.resultsPage.results.event
+    //     });
+    //   });
+  }
+
+  displayEvents(events) {
+    if (events.length > 0) {
+      events.map((item, index) => {
+        return (
+          <div className='showsItem' key={index}>
+            <div className='showText'>
+              <h3>{item.displayName}</h3>
+              <div>
+                {item.venue.displayName} | | {item.location.city}
+              </div>
+            </div>
+            <a href={item.uri} target='_blank' rel='noreferrer noopener'>
+              <button>Tickets</button>
+            </a>
+          </div>
+        );
       });
+    } else {
+      return (
+        <div>
+          <h2>More shows coming soon!</h2>
+        </div>
+      );
+    }
+  }
+
+  setContainerClass(events) {
+    if (events.length > 0) {
+      return 'shows';
+    } else {
+      return 'moreShows';
+    }
   }
 
   render() {
-    if (this.state.events.length > 0) {
-      return (
-        <div className="mainCont">
-          <h2>Upcoming Shows</h2>
-          <div className="shows">
-            {this.state.events.map((item, index) => {
-              return (
-                <div className="showsItem" key={index}>
-                  <div className="showText">
-                    <h3>{item.displayName}</h3>
-                    <div>
-                      {item.venue.displayName} | | {item.location.city}
-                    </div>
-                  </div>
-                  <a href={item.uri} target="_blank" rel="noreferrer noopener">
-                    <button>Tickets</button>
-                  </a>
-                </div>
-              );
-            })}
-            <span>
-              powered by
-              <a
-                href="https://www.songkick.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Songkick
-              </a>
-            </span>
-          </div>
+    // if (this.state.events) {
+    return (
+      <div className='mainCont'>
+        <h2>Upcoming Shows</h2>
+        <div className={this.setContainerClass(this.state.events)}>
+          {this.displayEvents(this.state.events)}
+          <span>
+            powered by
+            <a
+              href='https://www.songkick.com/'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              Songkick
+            </a>
+          </span>
         </div>
-      );
-    } else {
-      return (
-        <div className="mainCont">
-          <div className="showsLoading">
-            <h3>Loading shows...</h3>
-          </div>
-        </div>
-      );
-    }
+      </div>
+    );
+    // } else {
+    //   return (
+    //     <div className='mainCont'>
+    //       <div className='showsLoading'>
+    //         <h3>Loading shows...</h3>
+    //       </div>
+    //     </div>
+    //   );
+    // }
   }
 }
